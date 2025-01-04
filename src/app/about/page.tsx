4b738b80c37736498a898d3aba154847e4a08568
iqra-ikram <iqraikram58@gmail.com>
2025-01-05 02:07:41 +0500

    first commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,7 +11,7 @@ const Aboutpage = () => {
       <div className="text-center mb-16">
         <h1 className="text-5xl font-extrabold text-teal-800">About Our Blog</h1>
         <p className="text-lg text-gray-600 mt-4 leading-relaxed max-w-3xl mx-auto">
-          Welcome to Tech News, your go-to destination for everything related to modern web development, design, and emerging technologies. Whether you're a beginner or an advanced professional, our blog is designed to help you stay up-to-date with the latest trends, tools, and techniques in the tech world.
+          Welcome to Tech News, your go-to destination for everything related to modern web development, design, and emerging technologies. Whether you are a beginner or an advanced professional, our blog is designed to help you stay up-to-date with the latest trends, tools, and techniques in the tech world.
           <br />
           <br />
           We cover a wide array of topics, each aimed at helping developers, designers, and tech enthusiasts expand their knowledge and sharpen their skills. Our goal is to provide comprehensive guides, tutorials, and insightful articles. Stay ahead of the curve with our posts on the latest trends in web development. We explore new frameworks, tools, and technologies, as well as best practices for performance optimization, security, accessibility, and code quality.
@@ -22,7 +22,7 @@ const Aboutpage = () => {
       <div className="bg-white shadow-lg rounded-lg p-8 mb-12">
         <h2 className="text-3xl font-semibold text-teal-950">About Me</h2>
         <p className="text-lg text-gray-700 mt-4 leading-relaxed">
-          Hello! I'm Iqra Ikram, a passionate Frontend Developer with a deep love for building interactive and visually appealing web applications. I am currently a student at GIAIC, where I am exploring AgenticAI and GenAI. I am in my first year of study.
+          Hello I am Iqra Ikram, a passionate Frontend Developer with a deep love for building interactive and visually appealing web applications. I am currently a student at GIAIC, where I am exploring AgenticAI and GenAI. I am in my first year of study.
           <br />
           <br />
           As a developer, I am constantly driven by the desire to learn new technologies and improve my skills. My journey in web development began with learning the core technologies such as HTML, CSS, and JavaScript, TypeScript, and I have since expanded my knowledge to modern frameworks and libraries like React.js, Next.js, and TailwindCSS.
@@ -36,7 +36,7 @@ const Aboutpage = () => {
           Tech News is an emerging technology startup founded with the goal of revolutionizing the world of web development and design. As a team of passionate individuals, we strive to create innovative solutions that push the boundaries of modern technology and provide our clients with the tools they need to succeed in the digital age.
           <br />
           <br />
-          At Tech News, we are focused on leveraging cutting-edge technologies to deliver high-quality products and services. Our team is composed of individuals who have a strong passion for coding, designing, and developing solutions that are not only functional but also aesthetically pleasing and user-friendly.
+          At Tech News, we are focused on leveraging cutting-edge technologies to deliver high quality products and services. Our team is composed of individuals who have a strong passion for coding, designing, and developing solutions that are not only functional but also aesthetically pleasing and user-friendly.
         </p>
       </div>
 
@@ -45,25 +45,25 @@ const Aboutpage = () => {
         <h2 className="text-3xl font-semibold text-teal-950 mb-6">Why Choose Tech News?</h2>
         <ul className="space-y-6">
           <li className="text-lg text-gray-700">
-            <strong className="font-semibold text-blue-950">1. Expertise in Cutting-Edge Technologies:</strong> We stay ahead of the curve by mastering the latest technologies in the industry, such as React.js, Next.js, and AI solutions, to deliver robust and scalable solutions.
+            <strong className="font-semibold text-blue-950">1. Expertise in Cutting-Edge Technologies</strong> We stay ahead of the curve by mastering the latest technologies in the industry, such as React.js, Next.js, and AI solutions, to deliver robust and scalable solutions.
           </li>
           <li className="text-lg text-gray-700">
-            <strong className="font-semibold text-blue-950">2. High-Quality, User-Centered Design:</strong> We craft exceptional user experiences with responsive, and visually appealing designs, ensuring a seamless user journey on every platform.
+            <strong className="font-semibold text-blue-950">2. High-Quality, User-Centered Design</strong> We craft exceptional user experiences with responsive, and visually appealing designs, ensuring a seamless user journey on every platform.
           </li>
           <li className="text-lg text-gray-700">
-            <strong className="font-semibold text-blue-950">3. Focus on Performance & Scalability:</strong> From fast loading times to high availability, we ensure that your website or app delivers exceptional performance while scaling as your business grows.
+            <strong className="font-semibold text-blue-950">3. Focus on Performance & Scalability</strong> From fast loading times to high availability, we ensure that your website or app delivers exceptional performance while scaling as your business grows.
           </li>
           <li className="text-lg text-gray-700">
-            <strong className="font-semibold text-blue-950">4. Collaborative Approach:</strong> We believe in collaboration and transparency. We work closely with you to bring your ideas to life and ensure that the final product exceeds your expectations.
+            <strong className="font-semibold text-blue-950">4. Collaborative Approach</strong> We believe in collaboration and transparency. We work closely with you to bring your ideas to life and ensure that the final product exceeds your expectations.
           </li>
           <li className="text-lg text-gray-700">
             <strong className="font-semibold text-blue-950">5. Affordable & Timely Delivery:</strong> We deliver high-quality solutions on time and within budget, providing affordable pricing without compromising on quality.
           </li>
           <li className="text-lg text-gray-700">
-            <strong className="font-semibold text-blue-950">6. Continuous Support & Maintenance:</strong> We offer ongoing support, maintenance, and updates to ensure your product continues to perform at its best even after launch.
+            <strong className="font-semibold text-blue-950">6. Continuous Support & Maintenance</strong> We offer ongoing support, maintenance, and updates to ensure your product continues to perform at its best even after launch.
           </li>
           <li className="text-lg text-gray-700">
-            <strong className="font-semibold text-blue-950">7. Focused on Your Success:</strong> Your success is our success. We are committed to helping you achieve your goals by providing top-notch web development, design, and technology solutions.
+            <strong className="font-semibold text-blue-950">7. Focused on Your Success</strong> Your success is our success. We are committed to helping you achieve your goals by providing top-notch web development, design, and technology solutions.
           </li>
         </ul>
       </div>
